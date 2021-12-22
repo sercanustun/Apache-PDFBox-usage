750dd7c983531c872942662db55efb7e6307d706
Sercan <ustunsercan@gmail.com>
2021-12-22 10:51:38 +0300

    First commit

--- src/com/bilgeadam/boost/java/course01/lesson078/PDFBox.java
+++ src/com/bilgeadam/boost/java/course01/lesson078/PDFBox.java
@@ -3,8 +3,7 @@ package com.bilgeadam.boost.java.course01.lesson078;
 public class PDFBox {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("deneme");
 	}
 	
 }
